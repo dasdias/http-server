f67159aba8088f0c23b4a6b6e2284aaddfdcc85c
Alex Fix <dasdias@yandex.ru>
2023-03-10 09:00:19 +0300

    lesson01

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ const host = 'localhost';
 const port = 8000;
 
 const requestListener = (req, res) => {
-  if (req.url === '/get') {
-    res.writeHead(200);
-    res.end('success');
-  } else {
-    res.writeHead(405);
-    res.end('HTTP method not allowed');
-  }
+	console.log(res);
+	if (req.method === "GET") {
+		if (req.url === '/get') {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(200);
+			res.end('success');
+			return
+		}
+		res.setHeader('Content-Type', 'text/html');
+		res.writeHead(405);
+		res.end('HTTP method not allowed Привет');
+	}
+	// else if (req.method === "POST") {
+	// 	res.setHeader('Content-Type', 'text/html');
+	// 	res.writeHead(200);
+	// 	res.end('success');
+	// }
 
-  res.writeHead(405);
-  res.end('HTTP method not allowed');
+
+	else {
+		res.setHeader('Content-Type', 'text/html');
+		res.writeHead(405);
+		res.end('HTTP method not allowed');
+
+	}
+
 };
 
 const server = http.createServer(requestListener);
 
 server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+	console.log(`Server is running on http://${host}:${port}`);
 });
