781de3aa3332c68cb63806de196f4c25b4f76be1
Alexender <dasdias@yandex.ru>
2023-03-12 01:14:49 +0300

    lesson01

--- index.js
+++ index.js
@@ -1,34 +1,76 @@
 const http = require("http");
 const host = 'localhost';
 const port = 8000;
+const fs = require('fs');
+const path = require('node:path');
+
+const root = process.cwd()
+const curPath = path.join(__dirname, 'files')
 
 const requestListener = (req, res) => {
-	console.log(res);
-	if (req.method === "GET") {
-		if (req.url === '/get') {
+	if (req.url === '/get') {
+		if (req.method === "GET") {
+			try {
+				const curPath = path.join(__dirname, 'files')
+				const data = fs.readdirSync(curPath, 'utf8')
+				res.setHeader('Content-Type', 'text/html');
+				res.writeHead(200);
+				res.end(data.toString());
+			} catch (error) {
+				res.setHeader('Content-Type', 'text/html');
+				res.writeHead(500);
+				res.end('Internal server error');
+			}
+		} else {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(405);
+			res.end('HTTP method not allowed');
+		}
+	} else if (req.url === '/post') {
+		if (req.method === "POST") {
 			res.setHeader('Content-Type', 'text/html');
 			res.writeHead(200);
 			res.end('success');
-			return
+		} else {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(405);
+			res.end('HTTP method not allowed');
 		}
+	} else if (req.url === '/delete') {
+		if (req.method === "DELETE") {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(200);
+			res.end('success');
+		} else {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(405);
+			res.end('HTTP method not allowed');
+		}
+	} else if (req.url === '/redirect') {
+		if (req.method === "GET") {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(200);
+			res.end('<a href="/redirected">resource constantly available at /redirected</a>');
+		} else {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(405);
+			res.end('HTTP method not allowed');
+		}
+	} else if (req.url === '/redirected') {
+		if (req.method === "GET") {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(200);
+			res.end('<p>redirected page</p>');
+		} else {
+			res.setHeader('Content-Type', 'text/html');
+			res.writeHead(405);
+			res.end('HTTP method not allowed');
+		}
+	} else {
 		res.setHeader('Content-Type', 'text/html');
-		res.writeHead(405);
-		res.end('HTTP method not allowed Привет');
+		res.writeHead(404);
+		res.end('not found');
 	}
-	// else if (req.method === "POST") {
-	// 	res.setHeader('Content-Type', 'text/html');
-	// 	res.writeHead(200);
-	// 	res.end('success');
-	// }
-
-
-	else {
-		res.setHeader('Content-Type', 'text/html');
-		res.writeHead(405);
-		res.end('HTTP method not allowed');
-
-	}
-
 };
 
 const server = http.createServer(requestListener);
